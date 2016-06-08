7a5a01072304b346bc49e8dc5285e933f4c91241
Daniel <daniel.maldonado@gmail.com>
2016-06-08 03:59:42 -0500

    added basic dataFixtures

--- app/Resources/fixtures/PmgFixture.php
+++ app/Resources/fixtures/PmgFixture.php
@@ -1,5 +1,6 @@
 <?php
 use Sulu\Component\DocumentManager\DocumentManager;
+use Sulu\Bundle\ContentBundle\Document\BasePageDocument;
 use Sulu\Bundle\DocumentManagerBundle\DataFixtures\DocumentFixtureInterface;
 use Sulu\Component\Content\Document\WorkflowStage;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -22,6 +23,11 @@ class PmgFixture implements DocumentFixtureInterface, ContainerAwareInterface{
         return ['en','fr'];
     }
     
+    public function getDefaultLocale(){
+        
+        return 'en';
+    }
+        
     public function getOrder()
     {
         return 10;
@@ -40,32 +46,133 @@ class PmgFixture implements DocumentFixtureInterface, ContainerAwareInterface{
     {
         $directoryPath = $this->container->getParameter('kernel.root_dir');        
         $value = Yaml::parse(file_get_contents($directoryPath.'/Resources/fixtures/Data/content.yml'));
+        $sitesData = $value['sites'];
         
-        $locales = $this->getLocales();
         $homeDocuments = array();
         $webspaceManager = $this->container->get('sulu_core.webspace.webspace_manager');
         $this->documentManager = $documentManager;
         $webspaces = $webspaceManager->getWebspaceCollection();
         foreach($webspaces as $webspace){
             echo $webspace->getKey()."\n";
+            $webspaceContent = $sitesData[$webspace->getKey()];
+            
             $webspaceKey = $webspace->getKey();
-            $homeDocuments[$webspaceKey] = $this->populateHomeDocument(
+            $homeDocuments[$webspaceKey] = $this->generateHomepageDocuments(
                     $webspaceKey,
-                    $locales
+                    $webspaceContent['homepage']
                     );
             $this->generateHallsDocuments(
-                    $homeDocuments[$webspaceKey],
-                    $webspaceKey,
-                    $locales
+                    $homeDocuments[$webspaceKey]
                     );
             $this->generateMenusDocuments(
-                    $homeDocuments[$webspaceKey],
-                    $webspaceKey,
-                    $locales
+                    $homeDocuments[$webspaceKey]
+                    );
+            $this->generateTestimonialDocuments(
+                    $homeDocuments[$webspaceKey]
+                    );
+            $this->generateContactDocuments(
+                    $homeDocuments[$webspaceKey]
                     );
 
         }
         
+    }
+    
+    protected function loadPageFromData(BasePageDocument $document, $data, BasePageDocument $parentDocument = null){
+        
+        $documentManager    = $this->documentManager;
+        $locales            = $this->getLocales();
+        
+        $structureType      = $data['structureType'];
+        $documentContents   = $data['content'];
+        $defaultTitle       = isset($data['title']) ? $data['title'] : 'page';
+        $documentNavigation = isset($data['navigationContexts']) ? $data['navigationContexts']: [];
+        
+        $defaultLocale      = $this->getDefaultLocale();
+        $defaultData        = isset($documentContents[$defaultLocale]['fields']) ? $documentContents[$defaultLocale]['fields'] : null;
+        
+        $pathName           = $document->getPath();
+        
+        $document->setStructureType($structureType);
+        $document->setWorkflowStage(WorkflowStage::PUBLISHED);        
+
+        if(is_null($defaultData) && !empty($documentContents)){
+            foreach($locales as $localePosition){
+                if(isset($documentContents[$localePosition]['fields'])){
+                    $defaultData = $documentContents[$localePosition]['fields'];
+                    $defaultLocale = $localePosition;
+                    break;                    
+                }else{
+                    $defaultData = null;
+                }
+            }            
+        }        
+        
+        foreach($locales as $locale){
+            if(isset($documentContents[$locale]['fields']))
+            {
+                $documentData = $documentContents[$locale]['fields'];                
+            }
+            elseif(!is_null($defaultData))
+            {
+                $documentData = $defaultData;
+            }else{
+                $documentData = array();
+            }
+            $documentTitle = isset($documentContents[$locale]['title']) ? $documentContents[$locale]['title']: $defaultTitle;
+            
+            $document->setTitle($documentTitle);
+            $document->getStructure()->bind($documentData);
+            $document->setLocale($locale);
+            $options = [
+                'auto_create' => true,
+                'load_ghost_content'    => false
+            ];
+            $document->setNavigationContexts($documentNavigation);
+            
+            if((empty($documentData) && $locale !== $defaultLocale)
+                    || (isset($documentContents[$locale]['shadow']) ? ($documentContents[$locale]['shadow']) :  false)){
+                $document->setShadowLocale($defaultLocale);
+                $document->setShadowLocaleEnabled(true);                
+                $options['load_ghost_content'] = true;
+            }
+            
+            if(!is_null($parentDocument)){
+                $resource = $this->generateRLAction(
+                    $parentDocument->getUuid(),
+                    $document->getUuid(),
+                    array('title' => $documentTitle),
+                    $structureType,
+                    $parentDocument->getWebspaceName(),
+                    $locale
+                );
+                $pathName = $resource['resourceLocator'];
+                $document->setParent($parentDocument);
+                $document->setResourceSegment($resource['resourceLocator']);
+                $options['parent_path'] = $parentDocument->getPath();
+            }else{
+                $options['path'] = $pathName;
+            }
+            
+            $document->setExtension(
+                'excerpt',
+                [
+                    'title' => $defaultTitle,
+                    'description' => 'description',
+                    'categories' => [],
+                    'tags' => []
+                ]
+            );
+            
+            $documentManager->persist(
+                $document,
+                $locale,
+                $options
+            );
+            $documentManager->flush();
+        }
+        
+        return $document;        
     }
     
     /**
@@ -74,106 +181,90 @@ class PmgFixture implements DocumentFixtureInterface, ContainerAwareInterface{
      * @param string $name
      * @param string[] $locales
      */
-    protected function populateHomeDocument($name, $locales)
+    protected function generateHomepageDocuments($name, $data)
     {
         $documentManager = $this->documentManager;
         $pathName = '/cmf/'.$name.'/contents';
         $homeDocument = $documentManager->find($pathName);
-        $homeDocument->setTitle('Homepage');
-        $homeDocument->setStructureType('homepage');
-        $homeDocument->setWorkflowStage(WorkflowStage::PUBLISHED);
-        $homeDocument->getStructure()->bind(
-                array(
-                    'title' => 'Homepage',
-                    'slogan' => 'slogan',
-                    'slogan_text' => 'slogan text'
-            )
-        );
-        foreach($locales as $locale){
-            $documentManager->persist(
-                $homeDocument,
-                $locale,
-                [
-                    'path' => $pathName,
-                    'auto_create' => true,
-                    'load_ghost_content' => false,
-                ]
-            );
-            $documentManager->flush();
-        }
+                
+        $homeDocument = $this->loadPageFromData($homeDocument,$data);
         
         return $homeDocument;
     }
     
-    protected function generateHallsDocuments($parentDocument, $webspaceKey, $locales) {
-        $templateKey = 'halls';
-        $documentManager = $this->documentManager;
-        
+    protected function generateHallsDocuments(BasePageDocument $parentDocument, $data = null) {
+        $documentManager = $this->documentManager;        
         $page = $documentManager->create('page');
-        foreach($locales as $locale){
-            $page->setStructureType($templateKey);
-            $page->setTitle('Our Halls');
-            $page->setWorkflowStage(WorkflowStage::PUBLISHED);
-            $resource = $this->generateRLAction(
-                    $parentDocument->getUuid(),
-                    $page->getUuid(),
-                    array('title' => $page->getTitle()),
-                    $templateKey,
-                    $webspaceKey,
-                    $locale
-                    );
-            $page->setResourceSegment($resource['resourceLocator']);
-            $page->getStructure()->bind(array(
-                'title' => $page->getTitle()
-            ));
-            $page->setNavigationContexts(['main']);
-            $documentManager->persist($page, $locale, array(
-                'parent_path' => $parentDocument->getPath()
-            ));
-            
-            $documentManager->flush();
+        
+        if(is_null($data) || empty($data)){
+            $data = array();
+            $data['structureType'] = 'halls';
+            $data['content'] = array();
+            $data['title'] = 'Our Halls';
+            $data['navigationContexts'] = ['main'];
         }
-        //load halls foreach webspace
+        
+        $page = $this->loadPageFromData($page, $data, $parentDocument);
+        
+        return $page;
+        
     }
     
-    protected function generateMenusDocuments($parentDocument, $webspaceKey, $locales) {
+    protected function generateMenusDocuments(BasePageDocument $parentDocument, $data = null) {
         $templateKey = 'menus';
-        $documentManager = $this->documentManager;
-        
+        $documentManager = $this->documentManager;        
         $page = $documentManager->create('page');
-        foreach($locales as $locale){
-            $page->setStructureType($templateKey);
-            $page->setTitle('Our Menus');
-            $page->setWorkflowStage(WorkflowStage::PUBLISHED);
-            $resource = $this->generateRLAction(
-                    $parentDocument->getUuid(),
-                    $page->getUuid(),
-                    array('title' => $page->getTitle()),
-                    $templateKey,
-                    $webspaceKey,
-                    $locale
-                    );
-            $page->setResourceSegment($resource['resourceLocator']);
-            $page->getStructure()->bind(array(
-                'title' => $page->getTitle()
-            ));
-            $page->setNavigationContexts(['main']);
-            $documentManager->persist($page, $locale, array(
-                'parent_path' => $parentDocument->getPath()
-            ));
-            
-            $documentManager->flush();
+        
+        if(is_null($data) || empty($data)){
+            $data = array();
+            $data['structureType'] = $templateKey;
+            $data['content'] = array();
+            $data['title'] = 'Our Menus';
+            $data['navigationContexts'] = ['main'];
         }
-        //load halls foreach webspace
+        
+        $page = $this->loadPageFromData($page, $data, $parentDocument);
+        
+        return $page;
+
     }
     
-    protected function loadTestimonialPages(DocumentManager $documentManager){
+    protected function generateTestimonialDocuments(BasePageDocument $parentDocument, $data = null){
+        $templateKey = 'testimonials';
+        $documentManager = $this->documentManager;        
+        $page = $documentManager->create('page');
         
+        if(is_null($data) || empty($data)){
+            $data = array();
+            $data['structureType'] = $templateKey;
+            $data['content'] = array();
+            $data['title'] = 'Testimonials';
+            $data['navigationContexts'] = ['main'];
+        }
+        
+        $page = $this->loadPageFromData($page, $data, $parentDocument);
+        
+        return $page;
         
     }
     
-    protected function loadContactPages(){
+    protected function generateContactDocuments(BasePageDocument $parentDocument, $data = null){
         
+        $templateKey = 'contact';
+        $documentManager = $this->documentManager;        
+        $page = $documentManager->create('page');
+        
+        if(is_null($data) || empty($data)){
+            $data = array();
+            $data['structureType'] = $templateKey;
+            $data['content'] = array();
+            $data['title'] = 'Contact';
+            $data['navigationContexts'] = ['main'];
+        }
+        
+        $page = $this->loadPageFromData($page, $data, $parentDocument);
+        
+        return $page;
         
         
     }
@@ -186,7 +277,7 @@ class PmgFixture implements DocumentFixtureInterface, ContainerAwareInterface{
             $webspace = $webspaceManager->findWebspaceByKey($webspaceKey);
             $templateKey = $webspace->getTheme()->getDefaultTemplate(Structure::TYPE_PAGE);
         }
-
+        
         $result = $this->getResourceLocatorRepository()->generate(
             $parts,
             $parentUuid,
